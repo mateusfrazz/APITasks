1926c25be5d7615e642c4ba63f2e0de5e24e3a6f
mazeus <mateusfrazao12345@gmail.com>
2025-09-08 19:54:19 -0300

    definindo o bd

--- TodoList/src/main/java/br/com/mateus/TodoList/user/UserModel.java
+++ TodoList/src/main/java/br/com/mateus/TodoList/user/UserModel.java
@@ -1,10 +1,25 @@
 package br.com.mateus.TodoList.user;
 
+import java.util.UUID;
+import org.hibernate.annotations.CreationTimestamp;
+import jakarta.annotation.Generated;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.Data;
+import java.time.LocalDateTime;
 
 @Data
+@Entity(name = "tb_users")
 public class UserModel {
+    @Id // definindo a chave primaria
+    @Generated(value = "UUID") // definindo que o id sera gerado automaticamente
+    private UUID id;
+
     private String name;
     private String username;
     private String password;
+
+    // definindo que a data de criacao sera gerada automaticamente
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }
